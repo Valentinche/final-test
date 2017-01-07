42b5f6f69561ecc32bce5341d9f40711225723fe
Evgenia Manolova <emanolova@gmail.com>
2017-01-08 01:19:40 +0200

    initial commit

---
+++ task2/script.js
@@ -0,0 +1,18 @@
+"use strict"
+
+// задача 1
+
+var arr = [2, 5, 8, 4, 1, 12];
+
+// задача 2
+
+var text = "The quick brown fox jumps over the lazy dog";
+
+// задача 3
+
+
+// задача 4
+
+
+// задача 5
+
